011a1aa3399803ab7455368f0024380e902baafa
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-01-25 10:37:16 +0000

    balling

--- main.py
+++ main.py
@@ -38,8 +38,27 @@ def format_table(data: list[str]) -> dict:
              })
     return teams
 
+def form_dict(data: list[dict]) -> list[dict]:
+    form = []
+    for team in data:
+        results = []
+        for result in team['form'].split(' '):
+            if "w" in result.lower():
+                pass
+
+        # form.append({
+        #       "team":
+        #       "game -1":
+        #       "game -2":
+        #       "game -3":
+        #       "game -4":
+        #       "game -5":
+        #       "game -6":
+        #  })
 
 if __name__ == "__main__":
     team_data = get_table('https://www.bbc.co.uk/sport/football/premier-league/table')
     teams_dict = format_table(team_data)
-    print(pd.DataFrame(teams_dict))
+    for team in teams_dict:
+        print(team["form"])
+    form_dict(teams_dict)
